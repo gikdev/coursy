73f3f9c0fac27631bc4026c0ad4fdb94a4867823
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-06-01 00:57:44 +0330

    Fixed routing error

--- src/components/nav.component.jsx
+++ src/components/nav.component.jsx
@@ -6,26 +6,26 @@ export default function Nav() {
     <>
       <nav className="navbar bg-base-100">
         <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">
+          <Link to="/coursy/" className="btn btn-ghost text-xl">
             <span>Coursy</span>
           </Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1 items-center">
             <li>
-              <Link className="gap-1" to="/settings">
+              <Link className="gap-1" to="/coursy/settings">
                 <GearSix weight="duotone" size={20} color="currentColor" />
                 <span className="hidden sm:inline-block">Settings</span>
               </Link>
             </li>
             <li>
-              <Link className="gap-1" to="/about">
+              <Link className="gap-1" to="/coursy/about">
                 <Info weight="duotone" size={20} color="currentColor" />
                 <span className="hidden sm:inline-block">About</span>
               </Link>
             </li>
             <li>
-              <Link to="/profile" className="btn btn-ghost btn-circle avatar">
+              <Link to="/coursy/profile" className="btn btn-ghost btn-circle avatar">
                 <div className="size-10 rounded-full">
                   <img alt="Avatar image of user" src="/coursy/avatar.png" />
                 </div>
